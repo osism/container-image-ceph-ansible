6318fb98e5105a54caf7dfd02af2fdd7712a4ba8
Christian Berendt <berendt@betacloud-solutions.de>
2020-08-16 15:04:55 +0200

    Fix trivy script
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/security.sh
+++ scripts/security.sh
@@ -2,10 +2,15 @@
 
 # Available environment variables
 #
+# CEPH_VERSION
 # REPOSITORY
 # VERSION
 
+CEPH_VERSION=${CEPH_VERSION:-nautilus}
+REPOSITORY=${REPOSITORY:-osism/ceph-ansible}
 VERSION=${VERSION:-latest}
+
+COMMIT=$(git rev-parse --short HEAD)
 
 sudo apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
@@ -13,5 +18,11 @@ echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main |
 sudo apt-get update
 sudo apt-get install trivy
 
+if [[ $CEPH_VERSION == "master" ]]; then
+    tag=$REPOSITORY:latest
+else
+    tag=$REPOSITORY:$CEPH_VERSION
+fi
+
 trivy image --clear-cache
-trivy image $REPOSITORY:$VERSION
+trivy image "$tag-$COMMIT"
