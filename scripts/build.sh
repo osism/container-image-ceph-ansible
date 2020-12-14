67a65643272ced6cb3c6f2fbb88d21d7727dcaa5
Christian Berendt <berendt@betacloud-solutions.de>
2020-12-14 19:10:06 +0100

    github: use buildx (#79)
    Signed-off-by: Christian Berendt <berendt@betacloud-solutions.de>

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,7 +31,8 @@ else
     TAG=$REPOSITORY:$CEPH_VERSION
 fi
 
-docker build \
+docker buildx build \
+    --load \
     --build-arg "CEPH_VERSION=$CEPH_VERSION" \
     --build-arg "UBUNTU_VERSION=$UBUNTU_VERSION" \
     --build-arg "VERSION=$VERSION" \
@@ -44,6 +45,5 @@ docker build \
     --label "org.opencontainers.image.url=https://www.osism.de" \
     --label "org.opencontainers.image.vendor=Betacloud Solutions GmbH" \
     --label "org.opencontainers.image.version=$VERSION" \
-    --no-cache \
     --tag "$TAG-$(git rev-parse --short HEAD)" \
     $BUID_OPTS .
