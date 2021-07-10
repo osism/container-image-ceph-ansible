d26c7ef3ecdb20d33cfe1fb76794c6ec7fa6d167
Christian Berendt <berendt@osism.tech>
2021-07-10 14:55:51 +0200

    Get Ceph version from release repository for release builds
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,6 +19,12 @@ REPOSITORY=${REPOSITORY:-osism/ceph-ansible}
 REVISION=$(git rev-parse --short HEAD)
 VERSION=${VERSION:-latest}
 
+# NOTE: For builds for a specific release, the Ceph version is taken from the release repository.
+if [[ $VERSION != "latest" ]]; then
+    filename=$(curl -L https://raw.githubusercontent.com/osism/release/master/$VERSION/ceph.yml)
+    CEPH_VERSION=$(curl -L https://raw.githubusercontent.com/osism/release/master/$VERSION/$filename.yml | grep "ceph_version:" | awk -F': ' '{ print $2 }')
+fi
+
 . defaults/$CEPH_VERSION.sh
 
 if [[ -n $DOCKER_REGISTRY ]]; then
