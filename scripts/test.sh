af13ee7f2ab809ca2c7fd3ca15b0ed70bb7353f7
Christian Berendt <berendt@osism.tech>
2021-10-13 15:57:54 +0200

    Use the main branch where necessary
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/test.sh
+++ scripts/test.sh
@@ -26,8 +26,8 @@ COMMIT=$(git rev-parse --short HEAD)
 
 # NOTE: For builds for a specific release, the Ceph version is taken from the release repository.
 if [[ $VERSION != "latest" ]]; then
-    filename=$(curl -L https://raw.githubusercontent.com/osism/release/master/$VERSION/ceph.yml)
-    CEPH_VERSION=$(curl -L https://raw.githubusercontent.com/osism/release/master/$VERSION/$filename | grep "ceph_version:" | awk -F': ' '{ print $2 }')
+    filename=$(curl -L https://raw.githubusercontent.com/osism/release/main/$VERSION/ceph.yml)
+    CEPH_VERSION=$(curl -L https://raw.githubusercontent.com/osism/release/main/$VERSION/$filename | grep "ceph_version:" | awk -F': ' '{ print $2 }')
 fi
 
 . defaults/$CEPH_VERSION.sh
