447eafd38f0d5177b0c46d47019b6bce336eda30
Christian Berendt <berendt@osism.tech>
2021-11-17 20:10:06 +0100

    Copy versions.yml to /interface (#155)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p /interface/ceph-ansible
+mkdir -p /interface/ceph-ansible /interface/versions
 rsync -am --exclude='requirements*.yml' --include='*.yml' --exclude='*' /ansible/ /interface/ceph-ansible/
+cp /ansible/group_vars/all/versions.yml /interface/versions/ceph-ansible.yml
+
 exec /usr/bin/dumb-init -- "$@"
