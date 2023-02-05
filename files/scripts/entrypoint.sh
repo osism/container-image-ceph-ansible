e454e2a1162c2e346b988017778b3db2c14b03c2
Christian Berendt <berendt@osism.tech>
2023-02-05 12:12:38 +0100

    Render playbooks
    This script writes a list of all existing playbooks
    to /ansible/playbooks.yml.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p /interface/ceph-ansible /interface/versions
+mkdir -p /interface/ceph-ansible /interface/versions /interface/playbooks
 rsync -am --exclude='requirements*.yml' --include='*.yml' --exclude='*' /ansible/ /interface/ceph-ansible/
 cp /ansible/group_vars/all/versions.yml /interface/versions/ceph-ansible.yml
+cp /ansible/playbooks.yml /interface/playbooks/ceph-ansible.yml
 
 exec /usr/bin/dumb-init -- "$@"
