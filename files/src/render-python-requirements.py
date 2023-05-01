4c42648ce27ac127bfb8bd354cd768efbbdc1be5
Christian Berendt <berendt@osism.tech>
2023-05-01 20:13:06 +0200

    For version 4.3.0 we still have to install ansible instead of ansible-core
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/src/render-python-requirements.py
+++ files/src/render-python-requirements.py
@@ -25,8 +25,10 @@ environment = jinja2.Environment(loader=loader)
 
 template = environment.get_template("requirements.txt.j2")
 result = template.render({
+  'ansible_version': ceph_versions['ansible_version'],
   'ansible_core_version': ceph_versions['ansible_core_version'],
-  'osism_projects': versions['osism_projects']
+  'osism_projects': versions['osism_projects'],
+  'version': VERSION,
 })
 with open("/requirements.txt", "w+") as fp:
     fp.write(result)
