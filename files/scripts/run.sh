71c5bac2d4b48b2947e6efa06ccc3861b2473821
Christian Berendt <berendt@osism.tech>
2023-08-02 16:32:53 +0200

    Support /inventory/ansible/ansible.cfg (#359)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/scripts/run.sh
+++ files/scripts/run.sh
@@ -25,7 +25,9 @@ if [[ -e /ansible/ara.env ]]; then
 fi
 
 export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/ansible.cfg
-if [[ -e $ENVIRONMENTS_DIRECTORY/$SUB/ansible.cfg ]]; then
+if [[ -e /inventory/ansible/ansible.cfg ]]; then
+    export ANSIBLE_CONFIG=/inventory/ansible/ansible.cfg
+elif [[ -e $ENVIRONMENTS_DIRECTORY/$SUB/ansible.cfg ]]; then
     export ANSIBLE_CONFIG=$ENVIRONMENTS_DIRECTORY/$SUB/ansible.cfg
 fi
 
