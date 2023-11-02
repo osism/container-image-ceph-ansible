844fd16f76f594ac1357827c56d97c24c7db846e
Christian Berendt <berendt@osism.tech>
2023-11-02 21:28:42 +0100

    Revert "Remove pacific (#368)"
    This is necessary for the moment so that the upgrade tests for Ceph
    in osism/testbed are still functional.
    
    This reverts commit 2bff3a4c0d2c235ead2440fe3960f23308422e2b.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- files/src/render-versions.py
+++ files/src/render-versions.py
@@ -5,7 +5,7 @@ import yaml
 
 # get environment parameters
 
-CEPH_VERSION = os.environ.get("CEPH_VERSION", "quincy")
+CEPH_VERSION = os.environ.get("CEPH_VERSION", "pacific")
 VERSION = os.environ.get("VERSION", "latest")
 
 # load versions files from release repository
